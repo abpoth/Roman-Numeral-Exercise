9bb7371a6b7b162c9e8fe28761cfd506232424fe
abpoth <alexi.poth@yahoo.com>
2023-11-06 16:23:50 -0500

    Finished solution ready for review

--- roman-numerals/roman_numerals.c
+++ roman-numerals/roman_numerals.c
@@ -1,1 +1,43 @@
 #include "roman_numerals.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    unsigned int base;
+    const char* numeral;
+    const int size;
+} Roman_Numeral_Values_t;
+
+static const Roman_Numeral_Values_t romanNumeralTable[]=
+{
+    { 1000, "M" , 1},
+    { 900, "CM", 2},
+    { 500, "D" , 1},
+    { 400, "CD", 2},
+    { 100, "C" , 1},
+    { 90, "XC" , 2},
+    { 50, "L" , 1},
+    { 40, "XL" , 2},
+    { 10, "X" , 1},
+    { 9, "IX" , 2},
+    { 5, "V" , 1},
+    { 4, "IV" , 2},
+    { 1, "I" , 1}
+};
+
+char *to_roman_numeral(unsigned int number)
+{
+    char *result = (char*)calloc(16, sizeof(char)); // The largest Roman Numerals would be a max of 15 bytes
+    char *answer = result;
+
+    for(size_t i = 0; i < sizeof(romanNumeralTable) / sizeof(romanNumeralTable[0]); i++){
+        while (number >= romanNumeralTable[i].base){
+            strcpy(result, romanNumeralTable[i].numeral);
+            result += romanNumeralTable[i].size;
+            number -= romanNumeralTable[i].base;
+        }
+    }
+    free(result = NULL);
+    return answer;
+}
--- roman-numerals/roman_numerals.h
+++ roman-numerals/roman_numerals.h
@@ -1,6 +1,9 @@
 #ifndef ROMAN_NUMERALS_H
 #define ROMAN_NUMERALS_H
 
+/*!
+ * @param number // integer input to be convert to roman numeral
+ */
 char *to_roman_numeral(unsigned int number);
 
 #endif
