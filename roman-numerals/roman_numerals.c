4cd7081a0ce7dfc088551c35b5ca3dea7f8bf3c9
abpoth <alexi.poth@yahoo.com>
2023-11-14 12:17:52 -0500

    reduced roman numeral table size per comments

--- roman-numerals/roman_numerals.c
+++ roman-numerals/roman_numerals.c
@@ -4,40 +4,41 @@
 #include <string.h>
 
 typedef struct{
-    unsigned int base;
+    u_int16_t base;
     const char* numeral;
-    const int size;
+    //const int size;
 } Roman_Numeral_Values_t;
 
 static const Roman_Numeral_Values_t romanNumeralTable[]=
 {
-    { 1000, "M" , 1},
-    { 900, "CM", 2},
-    { 500, "D" , 1},
-    { 400, "CD", 2},
-    { 100, "C" , 1},
-    { 90, "XC" , 2},
-    { 50, "L" , 1},
-    { 40, "XL" , 2},
-    { 10, "X" , 1},
-    { 9, "IX" , 2},
-    { 5, "V" , 1},
-    { 4, "IV" , 2},
-    { 1, "I" , 1}
+    { 1000, "M"},
+    { 900, "CM"},
+    { 500, "D"},
+    { 400, "CD"},
+    { 100, "C"},
+    { 90, "XC"},
+    { 50, "L"},
+    { 40, "XL"},
+    { 10, "X"},
+    { 9, "IX"},
+    { 5, "V"},
+    { 4, "IV"},
+    { 1, "I"}
 };
 
 char *to_roman_numeral(unsigned int number)
 {
-    char *result = (char*)calloc(16, sizeof(char)); // The largest Roman Numerals would be a max of 15 bytes
+    char *result = calloc(16, sizeof(char)); // The largest Roman Numerals would be a max of 15 bytes
     char *answer = result;
 
     for(size_t i = 0; i < sizeof(romanNumeralTable) / sizeof(romanNumeralTable[0]); i++){
         while (number >= romanNumeralTable[i].base){
             strcpy(result, romanNumeralTable[i].numeral);
-            result += romanNumeralTable[i].size;
+            result += strlen(romanNumeralTable[i].numeral);
             number -= romanNumeralTable[i].base;
         }
     }
+
     free(result = NULL);
-    return answer;
+    return answer; 
 }
